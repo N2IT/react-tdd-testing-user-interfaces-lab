7d3b507d9b8fbd073de0180bdf91ac16d9b001a0
Tony Eder <tonyeder11@gmail.com>
2024-01-05 15:46:16 -0800

    in the green stage for all

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ function App() {
     <h1>
       Hi, I'm Tony
     </h1>
+    <img src="https://media.licdn.com/dms/image/D4D03AQFBRBoW2S9Xew/profile-displayphoto-shrink_800_800/0/1701297655076?e=1709769600&v=beta&t=5owbyirfUVj8i9sfpY2PkT72l7u1rpjWRVeZxPm_Y1c" alt="This is a picture of me"></img>
+    <h2>All About Me</h2>
+    <a href="https://github.com/N2IT">My GitHub Pages</a>
+    <a href="https://www.linkedin.com/in/tony-eder/">My LinkedIn Pages</a>
   </div>;
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -20,3 +20,51 @@ test("displays a top-level heading with the text `Hi, I'm Tony`", ()=> {
     //Assert
     expect(topLevelHeading).toBeInTheDocument();
 });
+
+test("displays an image of myself with alt text identifying content of the image", () => {
+    //Arrange
+    render (<App />)
+
+    //Act
+    const imageOfMe = screen.getByAltText(/picture of me/i)
+    
+    //Assert
+    expect(imageOfMe).toBeInTheDocument()
+})
+
+test("displays a second-level heading with the text `About Me`", () => {
+    //Arrange
+    render (<App />)
+
+    //Act
+    const secondLevelHeading = screen.getByRole("heading", {
+        name: /about me/i,
+        exact: true,
+        level: 2,
+    })
+
+    //Assert 
+    expect(secondLevelHeading).toBeInTheDocument()
+})
+
+test("displays 1 link to my GitHub page", () =>{
+    //Arrange
+    render(<App />)
+
+    //Act
+    const gitHubLink = screen.getByRole("link", { name: /github/i })
+    
+    //Assert
+    expect(gitHubLink ).toBeInTheDocument()
+})
+
+test("displays 1 link to my linkedIn page", () =>{
+    //Arrange
+    render(<App />)
+
+    //Act
+    const linkedInLink = screen.getByRole("link", {name : /linkedin/i })
+    
+    //Assert
+    expect(linkedInLink).toBeInTheDocument()
+})
